9afe9b234763a766b4b06eb1797105a2f786478b
Ashok <ashoknath@Ashoks-MacBook-Pro.local>
2021-05-23 17:08:48 +1000

    Creating pizza house web app

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Route::get('/pizzas', function () {
+  // get data from a database
+  $pizzas = [
+    ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+    ['type' => 'volcano', 'base' => 'garlic crust'],
+    ['type' => 'veg supreme', 'base' => 'thin & crispy']
+  ];
+
+  $name = request('name');
+
+  return view('pizzas', [
+    'pizzas' => $pizzas, 
+    'name' => $name,
+    'age' => request('age'),
+  ]);
+});
+
+
+Route::get('/pizzas/{id}', function ($id) {
+  // use the $id variable to query the db for a record
+  return view('details', [
+    'id' => $id 
+  ]);
+});
